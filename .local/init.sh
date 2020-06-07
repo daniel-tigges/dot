cfc6f6eb285300b804488b5c76ac66eea87e63e3
Daniel Tigges <danieltigges@googlemail.com>
2020-06-07 12:34:24 +0200

    Init echos and home fix

--- .local/init.sh
+++ .local/init.sh
@@ -2,6 +2,7 @@
 
 # These scripts installs all required packages and setup
 # all dotfiles stored in my git repository.
+# Start the script from your home directory
 # Make sure to have arch properly installed and setup:
 #	- user with sudo privileges
 #	- package-groups base and base-devel has to be installed
@@ -13,12 +14,15 @@ LightDMConfig="/etc/lightdm/lightdm.conf"
 LightDMGreeterConfig="/etc/lightdm/lightdm-mini-greeter.conf"
 
 # Get dependent packages
+echo "Get package list"
 mkdir -p $HOME/.local
 curl -Lks https://raw.githubusercontent.com/daniel-tigges/dot/master/.local/packages > $HOME/.local/packages
 
 # Install dependent packages
+echo "Install packages"
 yay -Sy --needed --noconfirm - < $HOME/.local/packages
 
+echo "Check out dotfiles"
 git clone --bare https://github.com/daniel-tigges/dot.git $DotDirectory
 function config {
    /usr/bin/git --git-dir=$DotDirectory --work-tree=$HOME $@
@@ -26,24 +30,28 @@ function config {
 mkdir -p .config-backup
 config checkout
 if [ $? = 0 ]; then
-  echo "Checked out config.";
+  echo "Check out succesfull";
   else
     echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} sh -c 'mkdir -p .config-backup/{} && mv {} .config-backup/{}'
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} sh -c 'mkdir -p $HOME/.config-backup/{} && mv {} $HOME/.config-backup/{}'
 fi;
 config checkout
 config config status.showUntrackedFiles no
 
 # create directories and files
+echo "Create default directories"
 mkdir -p $HOME/.cache/zsh # to store history file
 mkdir -p $HOME/.config/wget # for wget configuration
 touch $HOME/.config/wget/wgetrc # for wget configuration
 
 # Enable lightdm
-systemctl enable lightdm
+echo "Enable lightdm"
+sudo systemctl enable lightdm
 # Configure lightdm and mini-greeter
+echo "Configure lightdm and greeter"
 sed -i "/user-authority-in-system-dir=/s/.*/user-authority-in-system-dir=true/" $LightDMConfig
 sed -i "/greeter-session=/s/.*/greeter-session=lightdm-mini-greeter/" $LightDMConfig
 sed -i "/user-session=/s/.*/user-session=bspwm/" $LightDMConfig
 cp $HOME/.local/share/lightdm-mini-greeter.conf $LightDMGreeterConfig
 
+echo "Done. Restart system..."
